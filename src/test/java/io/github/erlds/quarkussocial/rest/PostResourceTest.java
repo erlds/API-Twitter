dd6477604801824a64dff472e80d34aaa91ab228
evaristo <erlds5518@gmail.com>
2023-04-25 20:26:46 -0300

    Cria Teste para criacao de post

---
+++ src/test/java/io/github/erlds/quarkussocial/rest/PostResourceTest.java
@@ -0,0 +1,47 @@
+package io.github.erlds.quarkussocial.rest;
+
+import io.github.erlds.quarkussocial.domain.model.User;
+import io.github.erlds.quarkussocial.domain.repository.UserRepository;
+import io.github.erlds.quarkussocial.rest.dto.CreatePostRequest;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+@TestHTTPEndpoint(PostResource.class)
+class PostResourceTest {
+
+    @Inject
+    UserRepository userRepository;
+    Long userId;
+
+    @BeforeEach
+    @Transactional
+    public void setup(){
+        var user = new User();
+        user.setAge(30);
+        user.setName("Fulano");
+        userRepository.persist(user);
+        userId = user.getId();
+    }
+
+    @Test
+    @DisplayName("should create a post for a user")
+    public void createPostTest() {
+        var postRequest = new CreatePostRequest();
+        postRequest.setText("Some text");
+
+        given().contentType(ContentType.JSON).body(postRequest).pathParam("userId",userId)
+                .when().post()
+                .then().statusCode(201);
+    }
+}
