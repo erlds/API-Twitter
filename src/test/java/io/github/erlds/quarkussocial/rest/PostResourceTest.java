707814f11240facf66e4f5a519e1c313499c8bd3
evaristo <erlds5518@gmail.com>
2023-04-27 21:35:14 -0300

    Cria testes para listagem de posts

--- src/test/java/io/github/erlds/quarkussocial/rest/PostResourceTest.java
+++ src/test/java/io/github/erlds/quarkussocial/rest/PostResourceTest.java
@@ -1,11 +1,16 @@
 package io.github.erlds.quarkussocial.rest;
 
+import io.github.erlds.quarkussocial.domain.model.Follower;
+import io.github.erlds.quarkussocial.domain.model.Post;
 import io.github.erlds.quarkussocial.domain.model.User;
+import io.github.erlds.quarkussocial.domain.repository.FollowerRepository;
+import io.github.erlds.quarkussocial.domain.repository.PostRepository;
 import io.github.erlds.quarkussocial.domain.repository.UserRepository;
 import io.github.erlds.quarkussocial.rest.dto.CreatePostRequest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,7 +27,16 @@ class PostResourceTest {
 
     @Inject
     UserRepository userRepository;
+
+    @Inject
+    FollowerRepository followerRepository;
+
+    @Inject
+    PostRepository postRepository;
+
     Long userId;
+    Long userNotFollowerId;
+    Long userFollowerId;
 
     @BeforeEach
     @Transactional
@@ -32,6 +46,29 @@ class PostResourceTest {
         user.setName("Fulano");
         userRepository.persist(user);
         userId = user.getId();
+
+        Post post = new Post();
+        post.setText("Hello");
+        post.setUser(user);
+        postRepository.persist(post);
+
+        var userNotFollower = new User();
+        userNotFollower.setAge(33);
+        userNotFollower.setName("Cicrano");
+        userRepository.persist(userNotFollower);
+        userNotFollowerId = userNotFollower.getId();
+
+        var userFollower = new User();
+        userFollower.setAge(35);
+        userFollower.setName("Beltrano");
+        userRepository.persist(userFollower);
+        userFollowerId = userFollower.getId();
+
+        Follower follower = new Follower();
+        follower.setUser(user);
+        follower.setFollower(userFollower);
+
+        followerRepository.persist(follower);
     }
 
     @Test
@@ -56,5 +93,56 @@ class PostResourceTest {
         given().contentType(ContentType.JSON).body(postRequest).pathParam("userId",inexistentUserId)
                 .when().post()
                 .then().statusCode(404);
+    }
+
+    @Test
+    @DisplayName("should return 404 when user doesn't exist")
+    public void listPostsUserNotFoundTest() {
+        var inexistentUserId = 999;
+
+        given().pathParam("userId",inexistentUserId)
+                .when().get()
+                .then().statusCode(404);
+    }
+
+    @Test
+    @DisplayName("should return 400 when followerId header is not present")
+    public void listPostsFollowerHeaderNotSendTest() {
+        given().pathParam("userId",userId)
+                .when().get()
+                .then().statusCode(400)
+                .body(Matchers.is("followerId not found"));
+    }
+
+    @Test
+    @DisplayName("should return 400 when follower is not present")
+    public void listPostsFollowerNotFoundTest() {
+        var inexistentFollowerId = 999;
+
+        given().pathParam("userId",userId)
+                .header("followerId",inexistentFollowerId)
+                .when().get()
+                .then().statusCode(400)
+                .body(Matchers.is("Follower inexistent"));
+    }
+
+    @Test
+    @DisplayName("should return 403 when follower doesn't follow the user")
+    public void listPostNotAFollower() {
+        given().pathParam("userId",userId)
+                .header("followerId",userNotFollowerId)
+                .when().get()
+                .then().statusCode(403)
+                .body(Matchers.is("You can't see these posts"));
+    }
+
+    @Test
+    @DisplayName("should return posts")
+    public void listPostsTest() {
+        given().pathParam("userId",userId)
+                .header("followerId",userFollowerId)
+                .when().get()
+                .then().statusCode(200)
+                .body("size()", Matchers.is(1));
     }
 }
